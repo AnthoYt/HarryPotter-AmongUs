6df2fff4076550346326d1e5025858b5bd6df565
Antho_Off <113532396+AnthoYt@users.noreply.github.com>
2025-02-04 15:50:07 +0100

    Update InnerNetClient_Update.cs

--- src/Patches/InnerNetClient_Update.cs
+++ src/Patches/InnerNetClient_Update.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using HarmonyLib;
@@ -11,7 +11,6 @@ using UnityEngine;
 
 namespace HarryPotter.Patches
 {
-    
     [HarmonyPatch(typeof(InnerNetClient), nameof(InnerNetClient.Update))]
     class InnerNetClient_Update
     {
@@ -30,7 +29,7 @@ namespace HarryPotter.Patches
         {
             if (AmongUsClient.Instance.GameState == InnerNetClient.GameStates.Joined)
             {
-                if (Main.Instance.Config.SelectRoles)
+                if (Main.Instance?.Config?.SelectRoles == true) // Ajout de la vérification explicite
                 {
                     if (Input.GetKeyDown(KeyCode.Alpha1))
                         Main.Instance.RpcRequestRole("Harry");
@@ -44,64 +43,79 @@ namespace HarryPotter.Patches
                         Main.Instance.RpcRequestRole("Bellatrix");
                 }
             }
-            
+
+            // Vérification de Main.Instance avant d'appeler ReloadSettings
             Main.Instance?.Config?.ReloadSettings();
-                        
-            foreach (TextMeshPro lobbySettingTMP in Main.Instance.CustomOptions)
+
+            // Vérification de HudManager.Instance avant de manipuler l'UI
+            if (HudManager.InstanceExists && HudManager.Instance.GameSettings != null)
             {
-                if (!HudManager.InstanceExists || HudManager.Instance.GameSettings == null)
+                foreach (TextMeshPro lobbySettingTMP in Main.Instance.CustomOptions)
                 {
-                    lobbySettingTMP.gameObject.SetActive(false);
-                    continue;
+                    Vector2 pos = HudManager.Instance.GameSettings.transform.position;
+
+                    pos.x += lobbySettingTMP.renderedWidth / 2;
+                    pos.y -= lobbySettingTMP.renderedHeight / 2;
+                    pos.y -= HudManager.Instance.GameSettings.renderedHeight;
+                    pos.y -= lobbySettingTMP.renderedHeight * Main.Instance.CustomOptions.IndexOf(lobbySettingTMP);
+
+                    lobbySettingTMP.gameObject.transform.position = pos;
+                    lobbySettingTMP.gameObject.SetActive(HudManager.Instance.GameSettings.isActiveAndEnabled);
+
+                    string optionText = Main.Instance.GetOptionTextByName(lobbySettingTMP.gameObject.name);
+
+                    RectTransform lobbyTextTrans = lobbySettingTMP.gameObject.GetComponent<RectTransform>();
+                    lobbyTextTrans.sizeDelta = lobbySettingTMP.GetPreferredValues(optionText);
+
+                    lobbySettingTMP.text = optionText;
                 }
-
-                Vector2 pos = HudManager.Instance.GameSettings.transform.position;
-                
-                pos.x += lobbySettingTMP.renderedWidth / 2;
-                pos.y -= lobbySettingTMP.renderedHeight / 2;
-                pos.y -= HudManager.Instance.GameSettings.renderedHeight;
-                pos.y -= lobbySettingTMP.renderedHeight * Main.Instance.CustomOptions.IndexOf(lobbySettingTMP);
-                
-                lobbySettingTMP.gameObject.transform.position = pos;
-                lobbySettingTMP.gameObject.SetActive(HudManager.Instance.GameSettings.isActiveAndEnabled);
-
-                string optionText = Main.Instance.GetOptionTextByName(lobbySettingTMP.gameObject.name);
-
-                RectTransform lobbyTextTrans = lobbySettingTMP.gameObject.GetComponent<RectTransform>();
-                lobbyTextTrans.sizeDelta = lobbySettingTMP.GetPreferredValues(optionText);
-
-                lobbySettingTMP.text = optionText;
             }
 
+            // Réinitialisation des WorldItems
             if (!AmongUsClient.Instance.IsGameStarted && Main.Instance != null)
             {
-                foreach (WorldItem wItem in Main.Instance.AllItems) wItem.Delete();
-                DeluminatorWorld.HasSpawned = false;
-                MaraudersMapWorld.HasSpawned = false;
-                PortKeyWorld.HasSpawned = false;
-                TheGoldenSnitchWorld.HasSpawned = false;
-                GhostStoneWorld.HasSpawned = false;
-                ButterBeerWorld.HasSpawned = false;
-                ElderWandWorld.HasSpawned = false;
-                BasWorldItem.HasSpawned = false;
-                SortingHatWorld.HasSpawned = false;
-                PhiloStoneWorld.HasSpawned = false;
-                Main.Instance.CurrentStage = 0;
-                Main.Instance.AllItems.Clear();
-                Main.Instance.AllPlayers.Clear();
-                Main.Instance.PossibleItemPositions = Main.Instance.DefaultItemPositons;
-                TaskInfoHandler.Instance.AllInfo.Clear();
+                ResetWorldItems(); // Extraction de la logique dans une méthode séparée
                 return;
             }
 
+            // Mise à jour de la liste des joueurs
             foreach (PlayerControl player in PlayerControl.AllPlayerControls)
-                if (Main.Instance?.AllPlayers.Where(x => x?._Object == player).ToList().Count == 0)
+            {
+                if (Main.Instance?.AllPlayers?.Any(x => x?._Object == player) != true)
+                {
                     Main.Instance?.AllPlayers.Add(new ModdedPlayerClass(player, null, new List<Item>()));
+                }
+            }
 
-            foreach (ModdedPlayerClass player in Main.Instance?.AllPlayers.ToList())
-                if (player == null || player._Object == null || player._Object.Data.Disconnected) Main.Instance?.AllPlayers.Remove(player);
+            // Nettoyage des joueurs déconnectés
+            Main.Instance?.AllPlayers.RemoveAll(player => player == null || player._Object == null || player._Object.Data.Disconnected);
 
-            Main.Instance?.GetLocalModdedPlayer().Update();
+            // Mise à jour du joueur local modifié
+            Main.Instance?.GetLocalModdedPlayer()?.Update();
+        }
+
+        // Méthode pour réinitialiser les objets WorldItem
+        static void ResetWorldItems()
+        {
+            foreach (WorldItem wItem in Main.Instance.AllItems)
+                wItem.Delete();
+
+            DeluminatorWorld.HasSpawned = false;
+            MaraudersMapWorld.HasSpawned = false;
+            PortKeyWorld.HasSpawned = false;
+            TheGoldenSnitchWorld.HasSpawned = false;
+            GhostStoneWorld.HasSpawned = false;
+            ButterBeerWorld.HasSpawned = false;
+            ElderWandWorld.HasSpawned = false;
+            BasWorldItem.HasSpawned = false;
+            SortingHatWorld.HasSpawned = false;
+            PhiloStoneWorld.HasSpawned = false;
+
+            Main.Instance.CurrentStage = 0;
+            Main.Instance.AllItems.Clear();
+            Main.Instance.AllPlayers.Clear();
+            Main.Instance.PossibleItemPositions = Main.Instance.DefaultItemPositons;
+            TaskInfoHandler.Instance.AllInfo.Clear();
         }
     }
 }
