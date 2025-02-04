94e8bba77ad192b7da7dc9dda0aefdfa3b8f13da
Antho_Off <113532396+AnthoYt@users.noreply.github.com>
2025-02-04 15:53:03 +0100

    Update CustomNetworkTransform_FixedUpdate.cs

--- src/Patches/CustomNetworkTransform_FixedUpdate.cs
+++ src/Patches/CustomNetworkTransform_FixedUpdate.cs
@@ -1,4 +1,4 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using HarryPotter.Classes;
 using System.Linq;
 using UnityEngine;
@@ -10,37 +10,55 @@ namespace HarryPotter.Patches
     {
         static bool Prefix(CustomNetworkTransform __instance)
         {
+            // Vérification que Main.Instance et PlayerControl.LocalPlayer ne sont pas null
+            if (Main.Instance == null || PlayerControl.LocalPlayer == null)
+                return true; // On permet au code original de continuer s'il y a un problème d'initialisation
+
+            // Si ce n'est pas le propriétaire, on continue avec les actions suivantes
             if (!__instance.AmOwner)
             {
-                if (Main.Instance.AllPlayers.Any(x => x._Object.NetTransform == __instance && x.ControllerOverride == Main.Instance.GetLocalModdedPlayer())) return false;
-                
+                // Vérification que le joueur local est bien le contrôleur du transform
+                if (Main.Instance.AllPlayers.Any(x => x._Object.NetTransform == __instance && x.ControllerOverride == Main.Instance.GetLocalModdedPlayer()))
+                    return false; // Empêcher l'exécution du code de mise à jour pour ce cas particulier
+
+                // Vérification et interpolation du mouvement
                 if (__instance.interpolateMovement != 0f)
                 {
                     Vector2 vector = __instance.targetSyncPosition - __instance.body.position;
+
+                    // Si le déplacement est suffisamment grand, on le traite
                     if (vector.sqrMagnitude >= 0.0001f)
                     {
                         float num = __instance.interpolateMovement / __instance.sendInterval;
                         vector.x *= num;
                         vector.y *= num;
-                        if (PlayerControl.LocalPlayer)
+
+                        // Calcul de la vitesse pour le joueur local
+                        if (PlayerControl.LocalPlayer != null)
                         {
-                            float multiplier = 1f;
-                            var foundPlayers = Main.Instance.AllPlayers.FindAll(x => x._Object.NetTransform == __instance);
-                            if (foundPlayers.Count > 0)
-                                multiplier = foundPlayers.First().SpeedMultiplier;
-                            vector = Vector2.ClampMagnitude(vector, PlayerControl.LocalPlayer.MyPhysics.TrueSpeed * multiplier);
+                            // On suppose un multiplicateur de vitesse basé sur le premier joueur trouvé avec ce transform
+                            var foundPlayer = Main.Instance.AllPlayers.FirstOrDefault(x => x._Object.NetTransform == __instance);
+                            if (foundPlayer != null)
+                            {
+                                float multiplier = foundPlayer.SpeedMultiplier;
+                                vector = Vector2.ClampMagnitude(vector, PlayerControl.LocalPlayer.MyPhysics.TrueSpeed * multiplier);
+                            }
                         }
+
                         __instance.body.velocity = vector;
                     }
                     else
                     {
-                        __instance.body.velocity = Vector2.zero;
+                        __instance.body.velocity = Vector2.zero; // Aucune vitesse si trop petit mouvement
                     }
                 }
+
+                // Mise à jour de la position cible avec la vélocité
                 __instance.targetSyncPosition += __instance.targetSyncVelocity * Time.fixedDeltaTime * 0.1f;
-                return false;
+                return false; // Bloque l'exécution du code original
             }
-            return true;
+
+            return true; // Permet à l'exécution du code original de continuer si c'est le propriétaire
         }
     }
 }
