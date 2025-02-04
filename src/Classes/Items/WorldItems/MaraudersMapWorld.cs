77a2a70752d55f9d93747807d0cc969cd950f0ca
Antho_Off <113532396+AnthoYt@users.noreply.github.com>
2025-02-04 16:18:25 +0100

    Update MaraudersMapWorld.cs

--- src/Classes/Items/WorldItems/MaraudersMapWorld.cs
+++ src/Classes/Items/WorldItems/MaraudersMapWorld.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+using System.Linq;
 using UnityEngine;
 using hunterlib.Classes;
 
@@ -9,7 +9,7 @@ namespace HarryPotter.Classes.WorldItems
         public static System.Random ItemRandom { get; set; } = new System.Random();
         public static float ItemSpawnChance { get; set; } = 30;
         public static bool HasSpawned { get; set; }
-        
+
         public MaraudersMapWorld(Vector2 position)
         {
             this.Position = position;
@@ -18,28 +18,40 @@ namespace HarryPotter.Classes.WorldItems
             this.Name = "Marauder's Map";
         }
 
+        // Méthode pour spawn l'objet dans le monde
         public static void WorldSpawn()
         {
             if (!CanSpawn())
                 return;
-            
+
             if (!ShipStatus.Instance)
                 return;
 
+            // Choisir une position valide pour le spawn
             Vector2 pos = Main.Instance.GetAllApplicableItemPositions().Random();
-            Main.Instance.RpcSpawnItem(1, pos);
+            Main.Instance.RpcSpawnItem(1, pos);  // Demander au serveur de spawner l'objet
             HasSpawned = true;
         }
-        
+
+        // Vérification des conditions pour spawner l'objet
         public static bool CanSpawn()
         {
-            if (Main.Instance.AllItems.Where(x => x.Id == 1).ToList().Count > 0) return false;
+            // Vérifier si un Marauder's Map est déjà dans le monde
+            if (Main.Instance.AllItems.Any(x => x.Id == 1)) return false;
+
+            // Ne peut pas spawn si une réunion est en cours
             if (MeetingHud.Instance) return false;
+
+            // Vérifie si le jeu a commencé
             if (!AmongUsClient.Instance.IsGameStarted) return false;
+
+            // Vérification de la probabilité d'apparition
             if (ItemRandom.Next(0, 100000) > ItemSpawnChance) return false;
+
+            // Vérifie si l'objet a déjà été spawné
             if (HasSpawned) return false;
 
-            return true;
+            return true;  // L'objet peut apparaître si toutes les conditions sont validées
         }
     }
 }
