62286b29ddd44d6a97b9b1138a4aa8243038043c
Antho_Off <113532396+AnthoYt@users.noreply.github.com>
2025-02-04 16:14:00 +0100

    Update Deluminator.cs

--- src/Classes/Items/Deluminator.cs
+++ src/Classes/Items/Deluminator.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.CompilerServices;
+using System;
 using HarryPotter.Classes.WorldItems;
 using Hazel;
 
@@ -15,33 +15,52 @@ namespace HarryPotter.Classes.Items
             this.Name = "Deluminator";
             this.Tooltip = "Deluminator:\nToggles the status of the lights.";
         }
+
         public override void Use()
         {
+            // Vérification si l'hôte utilise l'objet
             if (AmongUsClient.Instance.AmHost)
                 DeluminatorWorld.HasSpawned = false;
             else
             {
+                // Envoi du message RPC pour l'utilisation de l'item
                 MessageWriter writer = AmongUsClient.Instance.StartRpc(PlayerControl.LocalPlayer.NetId, (byte)Packets.UseItem, SendOption.Reliable);
                 writer.Write(Id);
                 writer.EndMessage();
             }
+
+            // Suppression de l'objet après utilisation
             this.Delete();
-            
-            switch (Main.Instance.IsLightsSabotaged())
+
+            // Vérification de l'état des lumières (sabotées ou non)
+            if (Main.Instance.IsLightsSabotaged())
             {
-                case true:
-                    var switchSystem = ShipStatus.Instance.Systems[SystemTypes.Electrical].Cast<SwitchSystem>();
+                // Lumières sabotées - on les répare
+                var switchSystem = ShipStatus.Instance.Systems[SystemTypes.Electrical] as SwitchSystem;
+
+                if (switchSystem != null)
+                {
+                    // Réinitialisation de l'état des interrupteurs
                     switchSystem.ActualSwitches = switchSystem.ExpectedSwitches;
-                    MessageWriter writer = AmongUsClient.Instance.StartRpc(PlayerControl.LocalPlayer.NetId, (byte)Packets.FixLightsRpc, SendOption.Reliable);
-                    writer.EndMessage();
-                    break;
-                case false:
-                    byte b = 4;
-                    for (var i = 0; i < 5; i++)
-                        if (new System.Random().Next(0, 2) == 0)
-                            b |= (byte)(1 << i);
-                    ShipStatus.Instance.RpcRepairSystem(SystemTypes.Electrical, b | 128);
-                    break;
+
+                    // Envoi du message RPC pour réparer les lumières
+                    MessageWriter repairWriter = AmongUsClient.Instance.StartRpc(PlayerControl.LocalPlayer.NetId, (byte)Packets.FixLightsRpc, SendOption.Reliable);
+                    repairWriter.EndMessage();
+                }
+            }
+            else
+            {
+                // Lumières non sabotées - on déclenche un sabotage
+                byte b = 4;
+                for (var i = 0; i < 5; i++)
+                {
+                    // Créer un effet aléatoire de sabotage
+                    if (new Random().Next(0, 2) == 0)
+                        b |= (byte)(1 << i);
+                }
+
+                // Envoi du message RPC pour saboter les lumières
+                ShipStatus.Instance.RpcRepairSystem(SystemTypes.Electrical, b | 128);
             }
         }
     }
