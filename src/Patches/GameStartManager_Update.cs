4e434de51bc205fa95716ffde002477a0ef9855f
Antho_Off <113532396+AnthoYt@users.noreply.github.com>
2025-02-04 15:51:55 +0100

    Update GameStartManager_Update.cs

--- src/Patches/GameStartManager_Update.cs
+++ src/Patches/GameStartManager_Update.cs
@@ -1,4 +1,4 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using HarryPotter.Classes;
 using InnerNet;
 using System.Linq;
@@ -13,10 +13,31 @@ namespace HarryPotter.Patches
     {
         static void Postfix(GameStartManager __instance)
         {
-            //__instance.GameRoomName.transform.localPosition = new Vector3(0.75f, 4.15f);
-            __instance.MakePublicButton.transform.position = new Vector3(__instance.MakePublicButton.transform.position.x, __instance.MakePublicButton.transform.position.y, 0);
-            //__instance.PlayerCounter.transform.localPosition = new Vector3(0.3f, -1.1f);
-            //__instance.StartButton.transform.localPosition = new Vector3(0f, -0.4f);
+            // Vérification de l'existence de MakePublicButton avant de modifier sa position
+            if (__instance.MakePublicButton != null)
+            {
+                __instance.MakePublicButton.transform.position = new Vector3(
+                    __instance.MakePublicButton.transform.position.x, 
+                    __instance.MakePublicButton.transform.position.y, 
+                    0);
+            }
+
+            // Si tu souhaites décommenter d'autres parties, tu peux le faire en vérifiant si les objets existent
+            // par exemple :
+            // if (__instance.GameRoomName != null)
+            // {
+            //     __instance.GameRoomName.transform.localPosition = new Vector3(0.75f, 4.15f);
+            // }
+
+            // if (__instance.PlayerCounter != null)
+            // {
+            //     __instance.PlayerCounter.transform.localPosition = new Vector3(0.3f, -1.1f);
+            // }
+
+            // if (__instance.StartButton != null)
+            // {
+            //     __instance.StartButton.transform.localPosition = new Vector3(0f, -0.4f);
+            // }
         }
     }
 }
