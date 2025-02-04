ce2cf8225cdf3d4018ce9a1d9e3fe09936406dd3
Antho_Off <113532396+AnthoYt@users.noreply.github.com>
2025-02-04 16:14:50 +0100

    Update BasItem.cs

--- src/Classes/Items/BasItem.cs
+++ src/Classes/Items/BasItem.cs
@@ -1,21 +1,33 @@
-﻿namespace HarryPotter.Classes.Items
+namespace HarryPotter.Classes.Items
 {
-    public class BasItem : Item
+    public class BasiliskItem : Item // Renommé pour plus de clarté
     {
-        public BasItem(ModdedPlayerClass owner)
+        public BasiliskItem(ModdedPlayerClass owner)
         {
             Owner = owner;
             ParentInventory = owner.Inventory;
             Id = 7;
             Name = "Basilisk";
-            Tooltip = "";
+            Tooltip = "A dangerous item that stuns the player who uses it.";
             IsTrap = true;
         }
 
         public override void Use()
         {
-            Delete();
-            hunterlib.Classes.Coroutines.Start(Main.Instance.CoStunPlayer(Owner._Object));
+            // On vérifie que l'objet est valide avant de tenter de l'utiliser
+            if (Owner._Object != null)
+            {
+                // Suppression de l'item de l'inventaire
+                Delete();
+
+                // Démarre la coroutine pour étourdir le joueur
+                hunterlib.Classes.Coroutines.Start(Main.Instance.CoStunPlayer(Owner._Object));
+            }
+            else
+            {
+                // Optionnel: vous pourriez vouloir loguer ou gérer l'erreur ici
+                UnityEngine.Debug.LogError("L'objet du propriétaire est nul, impossible d'utiliser le Basilisk.");
+            }
         }
     }
 }
