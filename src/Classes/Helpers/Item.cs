fa1fb4725e12b119a4f256a68db28826e9ffc5c3
Antho_Off <113532396+AnthoYt@users.noreply.github.com>
2025-02-04 16:26:39 +0100

    Update Item.cs

--- src/Classes/Helpers/Item.cs
+++ src/Classes/Helpers/Item.cs
@@ -1,26 +1,31 @@
-﻿using System.Collections.Generic;
-using HarryPotter.Classes.Items;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace HarryPotter.Classes
 {
     public class Item
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public Sprite Icon { get; set; }
-        public List<Item> ParentInventory { get; set; }
-        public ModdedPlayerClass Owner { get; set; }
-        public bool IsSpecial { get; set; }
-        public bool IsTrap { get; set; }
-        public string Tooltip { get; set; }
-        
+        public int Id { get; set; } // L'identifiant unique de l'objet
+        public string Name { get; set; } // Le nom de l'objet
+        public Sprite Icon { get; set; } // L'icône associée à l'objet
+        public List<Item> ParentInventory { get; set; } // Inventaire parent de cet objet
+        public ModdedPlayerClass Owner { get; set; } // Le joueur qui possède l'objet
+        public bool IsSpecial { get; set; } // Indique si l'objet est spécial (fonctionnalité spéciale)
+        public bool IsTrap { get; set; } // Indique si l'objet est une piège
+        public string Tooltip { get; set; } // Description de l'objet à afficher dans l'interface
+
+        // Méthode virtuelle à surcharger dans des classes dérivées pour définir l'action de l'objet lorsqu'il est utilisé
         public virtual void Use() { }
-        
+
+        // Méthode pour supprimer l'objet de l'inventaire du propriétaire
         public void Delete()
         {
-            ParentInventory.Remove(this);
-            ParentInventory = null;
+            // Si l'inventaire parent existe, on enlève l'objet
+            if (ParentInventory != null)
+            {
+                ParentInventory.Remove(this);
+                ParentInventory = null; // Dissocie l'inventaire
+            }
         }
     }
 }
