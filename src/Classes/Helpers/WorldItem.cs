5d39253466085e718ee79bac9a5cefae36dbd91b
Antho_Off <113532396+AnthoYt@users.noreply.github.com>
2025-02-04 16:24:25 +0100

    Update WorldItem.cs

--- src/Classes/Helpers/WorldItem.cs
+++ src/Classes/Helpers/WorldItem.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using HarryPotter.Classes.Items;
 using Hazel;
@@ -17,19 +17,25 @@ namespace HarryPotter.Classes
         public GameObject ItemWorldObject { get; set; }
         public bool IsPickedUp { get; set; }
 
+        // Mise à jour de l'item, pour gérer la prise en charge du pick-up
         public void Update()
         {
-            if (ItemWorldObject == null) return;
-            if (PlayerControl.LocalPlayer.Data.IsDead) return;
-            if (PlayerControl.LocalPlayer.Data.Disconnected) return;
+            // Ne rien faire si l'objet n'existe pas ou si le joueur est mort ou déconnecté
+            if (ItemWorldObject == null || PlayerControl.LocalPlayer.Data.IsDead || PlayerControl.LocalPlayer.Data.Disconnected)
+                return;
+
+            // Si l'objet est déjà dans l'inventaire du joueur, ne pas essayer de le prendre
             if (Main.Instance.GetLocalModdedPlayer().HasItem(Id)) return;
-            if (!ItemWorldObject.GetComponent<SpriteRenderer>().bounds.Intersects(PlayerControl.LocalPlayer.myRend.bounds)) return;
-            
-            PickUp();
+
+            // Vérifier si l'objet est à portée du joueur pour un pick-up
+            if (ItemWorldObject.GetComponent<SpriteRenderer>().bounds.Intersects(PlayerControl.LocalPlayer.myRend.bounds))
+                PickUp();
         }
 
+        // Dessiner l'icône de l'objet dans le monde
         public virtual void DrawWorldIcon()
         {
+            // Créer l'objet s'il n'existe pas encore
             if (ItemWorldObject == null)
             {
                 ItemWorldObject = new GameObject();
@@ -37,20 +43,24 @@ namespace HarryPotter.Classes
                 ItemWorldObject.SetActive(true);
             }
 
+            // Configuration du SpriteRenderer pour afficher l'icône
             SpriteRenderer itemRender = ItemWorldObject.GetComponent<SpriteRenderer>();
             itemRender.enabled = true;
             itemRender.sprite = Icon;
-            itemRender.transform.localScale = new Vector2(0.5f, 0.5f);
+            itemRender.transform.localScale = new Vector2(0.5f, 0.5f);  // Ajuster la taille de l'icône
             ItemWorldObject.transform.position = Position;
         }
 
+        // Gérer le pick-up de l'objet
         public void PickUp()
         {
+            // Si l'on est l'hôte, attribuer l'objet directement
             if (AmongUsClient.Instance.AmHost)
                 Main.Instance.GiveGrabbedItem(this.Id);
             else
             {
-                this.Delete();
+                // Si ce n'est pas l'hôte, envoyer une requête RPC pour le pick-up
+                Delete();  // Supprimer l'objet localement
                 MessageWriter writer = AmongUsClient.Instance.StartRpc(PlayerControl.LocalPlayer.NetId, (byte)Packets.TryPickupItem, SendOption.Reliable);
                 writer.Write(PlayerControl.LocalPlayer.PlayerId);
                 writer.Write(this.Id);
@@ -58,11 +68,14 @@ namespace HarryPotter.Classes
             }
         }
 
+        // Supprimer l'objet du monde
         public void Delete()
         {
             IsPickedUp = true;
-            if (ItemWorldObject != null) ItemWorldObject.Destroy();
+            if (ItemWorldObject != null)
+                ItemWorldObject.Destroy();  // Détruire l'objet du monde
 
+            // Si l'on est l'hôte, informer les autres joueurs que l'objet a été détruit
             if (AmongUsClient.Instance.AmHost)
             {
                 MessageWriter writer = AmongUsClient.Instance.StartRpc(PlayerControl.LocalPlayer.NetId, (byte)Packets.DestroyItem, SendOption.Reliable);
