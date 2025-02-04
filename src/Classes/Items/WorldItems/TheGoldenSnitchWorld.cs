6a546a20c2a0313e98b1b32cd43634730e7b4b33
Antho_Off <113532396+AnthoYt@users.noreply.github.com>
2025-02-04 16:15:59 +0100

    Update TheGoldenSnitchWorld.cs

--- src/Classes/Items/WorldItems/TheGoldenSnitchWorld.cs
+++ src/Classes/Items/WorldItems/TheGoldenSnitchWorld.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using Hazel;
 using UnityEngine;
@@ -23,69 +23,86 @@ namespace HarryPotter.Classes.WorldItems
         }
 
         private double _time;
+
         public override void DrawWorldIcon()
         {
+            // Vérifie si l'objet du monde n'a pas été créé
             if (ItemWorldObject == null)
-            {                
+            {
+                // Création de l'objet du monde
                 System.Console.WriteLine("Creating new Item: " + Name);
-                ItemWorldObject = new GameObject();
-                ItemWorldObject.AddComponent<SpriteRenderer>();
-                ItemWorldObject.SetActive(true);
-
-                SpriteRenderer itemRender = ItemWorldObject.GetComponent<SpriteRenderer>();
-                itemRender.enabled = true;
-                itemRender.sprite = Icon;
-                itemRender.transform.localScale = new Vector2(0.5f, 0.5f);
+                ItemWorldObject = new GameObject(Name); // Ajout d'un nom à l'objet pour faciliter le débogage
+                var spriteRenderer = ItemWorldObject.AddComponent<SpriteRenderer>();
+                spriteRenderer.sprite = Icon;
+                spriteRenderer.enabled = true;
+                spriteRenderer.transform.localScale = new Vector2(0.5f, 0.5f);
                 ItemWorldObject.transform.position = Position;
 
-                Rigidbody2D itemRigid = ItemWorldObject.AddComponent<Rigidbody2D>();
-                BoxCollider2D itemCollider = ItemWorldObject.AddComponent<BoxCollider2D>();
-                
+                // Ajout du Rigidbody2D et Collider2D
+                var itemRigid = ItemWorldObject.AddComponent<Rigidbody2D>();
+                var itemCollider = ItemWorldObject.AddComponent<BoxCollider2D>();
+
+                // Paramétrage du collider et des propriétés physiques
                 itemCollider.autoTiling = false;
                 itemCollider.edgeRadius = 0;
                 itemCollider.size = Icon.bounds.size * 0.5f;
                 itemCollider.sharedMaterial = Main.Instance.Assets.SnitchMaterial;
                 ItemWorldObject.layer = 8;
 
-                itemRigid.velocity = Velocity;
+                itemRigid.velocity = Velocity; // Définir la vélocité initiale
             }
 
-            Rigidbody2D itemRigid2 = ItemWorldObject.GetComponent<Rigidbody2D>();
+            // Configuration du Rigidbody2D pour un mouvement lisse
+            var itemRigid2 = ItemWorldObject.GetComponent<Rigidbody2D>();
             itemRigid2.fixedAngle = true;
             itemRigid2.drag = 0;
             itemRigid2.angularDrag = 0;
             itemRigid2.inertia = 0;
             itemRigid2.gravityScale = 0;
-            
+
+            // Animation de rotation basée sur une sinusoïde
             _time += Time.deltaTime;
-            
-            var angle = (float) (25 + (25 * Math.Sin(_time)));
+            var angle = (float)(25 + (25 * Math.Sin(_time)));
             itemRigid2.rotation = angle - 32.5f;
         }
 
+        // Méthode statique pour faire apparaître l'objet dans le monde
         public static void WorldSpawn()
         {
+            // Vérifie les conditions de spawn avant de créer l'objet
             if (!CanSpawn())
                 return;
-            
+
             if (!ShipStatus.Instance)
                 return;
 
-            Vector2 pos = Main.Instance.GetAllApplicableItemPositions().Random();
-            Main.Instance.RpcSpawnItem(3, pos);
-            HasSpawned = true;
+            Vector2 pos = Main.Instance.GetAllApplicableItemPositions().Random(); // Sélectionne une position aléatoire
+            Main.Instance.RpcSpawnItem(3, pos); // Demande au serveur de faire apparaître l'objet
+            HasSpawned = true; // Marque l'objet comme ayant été spawné
         }
-        
+
+        // Conditions de spawn de l'objet
         public static bool CanSpawn()
         {
-            if (Main.Instance.AllItems.Where(x => x.Id == 3).ToList().Count > 0) return false;
+            // Vérifie si l'objet est déjà présent dans le monde
+            if (Main.Instance.AllItems.Where(x => x.Id == 3).Any()) return false;
+
+            // Ne peut pas apparaître en réunion
             if (MeetingHud.Instance) return false;
+
+            // Vérifie si le jeu a commencé
             if (!AmongUsClient.Instance.IsGameStarted) return false;
+
+            // Vérifie la probabilité d'apparition de l'objet
             if (ItemRandom.Next(0, 100000) > ItemSpawnChance) return false;
+
+            // Vérifie le stade du jeu (par exemple, ne spawn pas avant un certain stade)
             if (Main.Instance.CurrentStage < 2) return false;
+
+            // Vérifie si l'objet a déjà été spawné
             if (HasSpawned) return false;
 
-            return true;
+            return true; // Si toutes les conditions sont validées, on autorise le spawn
         }
     }
 }
