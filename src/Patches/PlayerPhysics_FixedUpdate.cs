ad9df949360b773587f0ca55f0a5d2532d0e86df
Antho_Off <113532396+AnthoYt@users.noreply.github.com>
2025-02-20 10:43:08 +0100

    Update PlayerPhysics_FixedUpdate.cs

--- src/Patches/PlayerPhysics_FixedUpdate.cs
+++ src/Patches/PlayerPhysics_FixedUpdate.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using HarmonyLib;
 using HarryPotter.Classes;
@@ -13,11 +13,23 @@ namespace HarryPotter.Patches
     {
         static bool Prefix(PlayerPhysics __instance)
         {
+            // Récupère le joueur moddé par son ID
             ModdedPlayerClass moddedController = Main.Instance.ModdedPlayerById(__instance.myPlayer.PlayerId);
-            if (__instance.myPlayer != PlayerControl.LocalPlayer || moddedController?.Role?.RoleName != "Bellatrix" || ((Bellatrix)moddedController.Role).MindControlledPlayer == null) return true;
+
+            // Vérifie si ce n'est pas le joueur local ou si ce n'est pas Bellatrix avec un joueur contrôlé
+            if (__instance.myPlayer != PlayerControl.LocalPlayer || moddedController?.Role?.RoleName != "Bellatrix" || ((Bellatrix)moddedController.Role).MindControlledPlayer == null)
+                return true;
+
+            // Récupère le PlayerPhysics du joueur contrôlé mentalement
             PlayerPhysics controlledPlayer = ((Bellatrix)moddedController.Role).MindControlledPlayer._Object.MyPhysics;
+            
+            // Définit la vitesse en fonction de l'entrée du joystick
             Vector2 vel = HudManager.Instance.joystick.Delta * __instance.TrueSpeed;
+            
+            // Applique la vitesse au joueur contrôlé
             controlledPlayer.body.velocity = vel;
+
+            // Crée un message RPC pour synchroniser la position et la vitesse du joueur contrôlé
             MessageWriter writer = AmongUsClient.Instance.StartRpc(PlayerControl.LocalPlayer.NetId, (byte)Packets.MoveControlledPlayer, SendOption.Reliable);
             writer.Write(controlledPlayer.myPlayer.PlayerId);
             writer.Write(vel.x);
@@ -25,14 +37,20 @@ namespace HarryPotter.Patches
             writer.Write(controlledPlayer.body.position.x);
             writer.Write(controlledPlayer.body.position.y);
             writer.EndMessage();
+
+            // Empêche l'exécution de la méthode originale (FixedUpdate) dans ce cas
             return false;
         }
 
         static void Postfix(PlayerPhysics __instance)
         {
+            // Si le joueur local est le propriétaire et que le joueur moddé existe
             if (__instance.AmOwner && Main.Instance.GetLocalModdedPlayer() != null)
             {
+                // Applique un éventuel renversement des contrôles directionnels
                 __instance.body.velocity *= Main.Instance.GetLocalModdedPlayer().ReverseDirectionalControls ? -1f : 1f;
+
+                // Applique un multiplicateur de vitesse (par exemple, si le modded player a une vitesse boostée)
                 __instance.body.velocity *= Main.Instance.GetLocalModdedPlayer().SpeedMultiplier;
             }
         }
