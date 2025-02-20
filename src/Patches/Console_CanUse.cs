6f9ab08c45f011e2949b07f9873543cbd533792d
Antho_Off <113532396+AnthoYt@users.noreply.github.com>
2025-02-20 10:41:17 +0100

    Update Console_CanUse.cs

--- src/Patches/Console_CanUse.cs
+++ src/Patches/Console_CanUse.cs
@@ -1,4 +1,4 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using HarryPotter.Classes;
 
 namespace HarryPotter.Patches
@@ -8,16 +8,19 @@ namespace HarryPotter.Patches
     {
         static void Postfix(GameData.PlayerInfo __0, ref bool __1, ref bool __2)
         {
+            // Recherche du joueur moddé
             ModdedPlayerClass moddedPlayer = Main.Instance.ModdedPlayerById(__0.PlayerId);
 
+            // Si aucun joueur moddé n'est trouvé, on ne fait rien
             if (moddedPlayer == null)
                 return;
-            
-            if (moddedPlayer.CanUseConsoles)
-                return;
-            
-            __1 = false;
-            __2 = false;
+
+            // Si le joueur moddé ne peut pas utiliser la console, on l'empêche
+            if (!moddedPlayer.CanUseConsoles)
+            {
+                __1 = false;
+                __2 = false;
+            }
         }
     }
 }
