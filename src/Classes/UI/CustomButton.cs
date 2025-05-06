3fc94659e74eaf1044b52ce7476ab51562462b00
Antho_Off <113532396+AnthoYt@users.noreply.github.com>
2025-05-06 10:52:52 +0200

    Update CustomButton.cs

--- src/Classes/UI/CustomButton.cs
+++ src/Classes/UI/CustomButton.cs
@@ -1,75 +1,67 @@
 using System;
-using System.Linq;
-using TMPro;
-using hunterlib.Classes;
+using Reactor.Utilities.Attributes;
 using UnityEngine;
 
-namespace HarryPotter.Classes.Helpers.UI
+namespace HarryPotter.Classes.Helpers.UI;
+
+public delegate void ClickEvent();
+
+[RegisterInIl2Cpp]
+public class CustomButton : MonoBehaviour
 {
-    public delegate void ClickEvent();
-    
-    [RegisterInIl2Cpp]
-    public class CustomButton : MonoBehaviour
+    public CustomButton(IntPtr ptr) : base(ptr)
     {
-        public event ClickEvent OnClick;
-        public event ClickEvent OnRightClick;
-        public Color HoverColor { get; set; }
-        public bool Enabled { get; set; }
-        public SpriteRenderer Renderer { get; set; }
+    }
 
-        public CustomButton(IntPtr ptr) : base(ptr) { }
+    public Color HoverColor { get; set; }
+    public bool Enabled { get; set; }
+    public SpriteRenderer Renderer { get; set; }
 
-        public void SetColor(Color color)
-        {
-            HoverColor = color;
-            if (Renderer != null)
-            {
-                Renderer.material.SetColor("_OutlineColor", color);
-            }
-        }
-        
-        private void Start()
-        {
-            Enabled = true;
-            Renderer = gameObject.GetComponent<SpriteRenderer>();
-            if (Renderer != null)
-            {
-                Renderer.material.shader = Shader.Find("Sprites/Outline");
-                Renderer.material.SetColor("_OutlineColor", HoverColor);
-            }
-            gameObject.AddComponent<BoxCollider2D>().isTrigger = true;
-        }
+    private void Start()
+    {
+        Enabled = true;
+        Renderer = gameObject.GetComponent<SpriteRenderer>();
+        Renderer.material.shader = Shader.Find("Sprites/Outline");
+        Renderer.material.SetColor("_OutlineColor", HoverColor);
+        gameObject.AddComponent<BoxCollider2D>().isTrigger = true;
+    }
 
-        private void Update()
-        {
-            if (!Enabled && Renderer != null)
-            {
-                Renderer.material.SetFloat("_Outline", 0f);
-            }
-        }
+    public void Update()
+    {
+        if (!Enabled) Renderer.material.SetFloat("_Outline", 0f);
+    }
 
-        private void OnMouseDown()
-        {
-            if (!Enabled) return;
-            OnClick?.Invoke();
-        }
+    private void OnMouseDown()
+    {
+        if (!Enabled) return;
+        OnClick?.Invoke();
+    }
 
-        public void OnMouseOver()
-        {
-            if (!Enabled || !Input.GetMouseButtonDown(1)) return;
-            OnRightClick?.Invoke();
-        }
+    private void OnMouseEnter()
+    {
+        if (!Enabled) return;
+        Renderer.material.SetFloat("_Outline", 1f);
+    }
 
-        private void OnMouseEnter()
-        {
-            if (!Enabled || Renderer == null) return;
-            Renderer.material.SetFloat("_Outline", 1f);
-        }
+    private void OnMouseExit()
+    {
+        if (!Enabled) return;
+        Renderer.material.SetFloat("_Outline", 0f);
+    }
 
-        private void OnMouseExit()
-        {
-            if (!Enabled || Renderer == null) return;
-            Renderer.material.SetFloat("_Outline", 0f);
-        }
+    public void OnMouseOver()
+    {
+        if (!Input.GetMouseButtonDown(1)) return;
+        if (!Enabled) return;
+        OnRightClick?.Invoke();
+    }
+
+    public event Action OnClick;
+    public event Action OnRightClick;
+
+    public void SetColor(Color color)
+    {
+        HoverColor = color;
+        Renderer.material.SetColor("_OutlineColor", color);
     }
 }
