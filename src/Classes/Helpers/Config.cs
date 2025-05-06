7639f35fef43dfe6a8fa43cc208fa033d2d53935
Antho_Off <113532396+AnthoYt@users.noreply.github.com>
2025-05-06 10:52:05 +0200

    Update Config.cs

--- src/Classes/Helpers/Config.cs
+++ src/Classes/Helpers/Config.cs
@@ -1,56 +1,139 @@
-using hunterlib.Classes;
+using HarryPotter.CustomOption;
+using System;
 
-namespace HarryPotter.Classes
+namespace HarryPotter.Classes;
+
+internal class Config
 {
-    class Config
+    private static CustomHeaderOption roleSettings;
+    private static CustomNumberOption BellatrixOn;
+    private static CustomNumberOption HarryOn;
+    private static CustomNumberOption HermioneOn;
+    private static CustomNumberOption RonOn;
+    private static CustomNumberOption VoldemortOn;
+
+    private static CustomHeaderOption HPSettings;
+    private static CustomToggleOption enableModRole;
+    private static CustomToggleOption spellsInVents;
+    private static CustomToggleOption randomGameStartPosition;
+
+    private static CustomHeaderOption ItemSettings;
+    private static CustomNumberOption beerDuration;
+    private static CustomNumberOption mapDuration;
+
+    private static CustomHeaderOption VoldemortSettings;
+    private static CustomToggleOption orderOfTheImp;
+    private static CustomToggleOption separateCooldowns;
+    private static CustomNumberOption crucioCooldown;
+    private static CustomNumberOption crucioDuration;
+    private static CustomHeaderOption BellatrixSettings;
+    private static CustomNumberOption imperioDuration;
+    private static CustomHeaderOption RonSettings;
+    private static CustomNumberOption defensiveDuelistCooldown;
+    private static CustomNumberOption defensiveDuelistDuration;
+    private static CustomHeaderOption HarrySettings;
+    private static CustomNumberOption invisCloakCooldown;
+    private static CustomNumberOption invisCloakDuration;
+    private static CustomHeaderOption HermioneSettings;
+    private static CustomNumberOption hourglassCooldown;
+    private static CustomNumberOption hourglassTimer;
+
+
+    public int EnableVoldemort { get; private set; }
+    public int EnableBellatrix { get; private set; }
+    public int EnableHarry { get; private set; }
+    public int EnableHermione { get; private set; }
+    public int EnableRon { get; private set; }
+
+    public bool OrderOfTheImp { get; private set; }
+    public float MapDuration { get; private set; }
+    public float DefensiveDuelistDuration { get; private set; }
+    public float InvisCloakDuration { get; private set; }
+    public float HourglassTimer { get; private set; }
+    public float BeerDuration { get; private set; }
+    public float CrucioDuration { get; private set; }
+    public float DefensiveDuelistCooldown { get; private set; }
+    public float InvisCloakCooldown { get; private set; }
+    public float HourglassCooldown { get; private set; }
+    public float CrucioCooldown { get; private set; }
+    public bool SpellsInVents { get; private set; }
+    public float ImperioDuration { get; private set; }
+    public bool SeparateCooldowns { get; private set; }
+    public bool EnableModRole { get; private set; }
+    public bool RandomGameStartPosition { get; private set; }
+    public static Func<object, string> PercentFormat { get; } = value => $"{value:0}%";
+    private static Func<object, string> CooldownFormat { get; } = value => $"{value:0.0#}s";
+    private static Func<object, string> MultiplierFormat { get; } = value => $"{value:0.0#}x";
+
+
+    public static void LoadOption()
     {
-        // Déclaration des options personnalisées pour la configuration du jeu
-        private CustomToggleOption Option1 = CustomToggleOption.Create("Order of the Impostors", false);
-        private CustomToggleOption Option3 = CustomToggleOption.Create("Can Spells be Used In Vents", false);
-        private CustomToggleOption Option4 = CustomToggleOption.Create("Show Info Popups/Tooltips");
-        private CustomToggleOption Option5 = CustomToggleOption.Create("Shared Voldemort Cooldowns");
+        var num = 0;
+        HPSettings = new CustomHeaderOption(num++, "HPSettings");
+        enableModRole = new CustomToggleOption(num++, "enableModRole");
+        orderOfTheImp = new CustomToggleOption(num++, "orderOfTheImp", false);
+        randomGameStartPosition = new CustomToggleOption(num++, "randomGameStartPosition");
 
-        private CustomNumberOption Option9 = CustomNumberOption.Create("Defensive Duelist Cooldown", 20f, 40f, 10, 2.5f);
-        private CustomNumberOption Option10 = CustomNumberOption.Create("Invisibility Cloak Cooldown", 20f, 40f, 10, 2.5f);
-        private CustomNumberOption Option11 = CustomNumberOption.Create("Time Turner Cooldown", 20f, 40f, 10f, 2.5f);
-        private CustomNumberOption Option12 = CustomNumberOption.Create("Crucio Cooldown", 20f, 40f, 10f, 2.5f);
+        roleSettings = new CustomHeaderOption(num++, "roleSettings");
+        VoldemortOn = new CustomNumberOption(num++, ModHelpers.cs(Palette.ImpostorRed, "RoleNameVoldemort"), 0f, 0f, 100f, 10f, PercentFormat);
+        BellatrixOn = new CustomNumberOption(num++, ModHelpers.cs(Palette.ImpostorRed, "RoleNameBellatrix"), 0f, 0f, 100f, 10f, PercentFormat);
+        HarryOn = new CustomNumberOption(num++, ModHelpers.cs(Palette.Orange, "RoleNameHarry"), 0f, 0f, 100f, 10f, PercentFormat);
+        HermioneOn = new CustomNumberOption(num++, ModHelpers.cs(Palette.Orange, "RoleNameHermione"), 0f, 0f, 100f, 10f, PercentFormat);
+        RonOn = new CustomNumberOption(num++, ModHelpers.cs(Palette.Orange, "RoleNameRon"), 0f, 0f, 100f, 10f, PercentFormat);
 
-        // Propriétés publiques en lecture seule qui accèdent aux options
-        public bool OrderOfTheImp { get; private set; }
-        public float MapDuration { get { return 10; } } // Durée fixe pour la carte
-        public float DefensiveDuelistDuration { get { return 10; } } // Durée fixe pour le duelliste défensif
-        public float InvisCloakDuration { get { return 10; } } // Durée fixe pour le manteau d'invisibilité
-        public float HourglassTimer { get { return 10; } } // Durée fixe pour le sablier
-        public float BeerDuration { get { return 10; } } // Durée fixe pour la bière
-        public float CrucioDuration { get { return 10; } } // Durée fixe pour Crucio
+        VoldemortSettings = new CustomHeaderOption(num++, ModHelpers.cs(Palette.ImpostorRed, "RoleNameVoldemort"));
+        spellsInVents = new CustomToggleOption(num++, "spellsInVents", false);
+        separateCooldowns = new CustomToggleOption(num++, "separateCooldowns");
 
-        // Propriétés publiques avec les valeurs des cooldowns
-        public float DefensiveDuelistCooldown { get; private set; }
-        public float InvisCloakCooldown { get; private set; }
-        public float HourglassCooldown { get; private set; }
-        public float CrucioCooldown { get; private set; }
+        BellatrixSettings = new CustomHeaderOption(num++, ModHelpers.cs(Palette.ImpostorRed, "RoleNameBellatrix"));
+        imperioDuration = new CustomNumberOption(num++, "imperioDuration", 5f, 5f, 30f, 1f, CooldownFormat);
+        crucioCooldown = new CustomNumberOption(num++, "crucioCooldown", 20f, 40f, 10f, 2.5f, CooldownFormat);
+        crucioDuration = new CustomNumberOption(num++, "crucioDuration", 5f, 5f, 30f, 1f, CooldownFormat);
 
-        // Propriétés pour les options de configuration
-        public bool SpellsInVents { get; private set; }
-        public float ImperioDuration { get { return 10; } } // Durée fixe pour Imperio
-        public bool ShowPopups { get; private set; }
-        public bool SeparateCooldowns { get; private set; }
-        public bool SimplerWatermark { get { return false; } } // Valeur par défaut pour la marque d'eau simplifiée
-        public bool SelectRoles { get { return false; } } // Valeur par défaut pour la sélection des rôles
-        public bool UseCustomRegion { get { return false; } } // Valeur par défaut pour l'utilisation de régions personnalisées
+        RonSettings = new CustomHeaderOption(num++, ModHelpers.cs(Palette.Orange, "RoleNameRon"));
+        defensiveDuelistCooldown = new CustomNumberOption(num++, "defensiveDuelistCooldown", 20f, 40f, 10, 2.5f, CooldownFormat);
+        defensiveDuelistDuration = new CustomNumberOption(num++, "defensiveDuelistDuration", 5f, 5f, 30f, 1f, CooldownFormat);
 
-        // Méthode pour recharger les paramètres et mettre à jour les valeurs des options
-        public void ReloadSettings()
-        {
-            // Mettre à jour les valeurs des options à partir des paramètres actuels
-            OrderOfTheImp = Option1.Value;
-            SpellsInVents = Option3.Value;
-            DefensiveDuelistCooldown = Option9.Value;
-            InvisCloakCooldown = Option10.Value;
-            HourglassCooldown = Option11.Value;
-            CrucioCooldown = Option12.Value;
-            ShowPopups = Option4.Value;
-            SeparateCooldowns = !Option5.Value; // Inverser la valeur de l'option "Shared Voldemort Cooldowns"
-        }
+        HarrySettings = new CustomHeaderOption(num++, ModHelpers.cs(Palette.Orange, "RoleNameHarry"));
+        invisCloakCooldown = new CustomNumberOption(num++, "invisCloakCooldown", 20f, 40f, 10, 2.5f, CooldownFormat);
+        invisCloakDuration = new CustomNumberOption(num++, "invisCloakDuration", 5f, 5f, 30f, 1f, CooldownFormat);
+
+        HermioneSettings = new CustomHeaderOption(num++, ModHelpers.cs(Palette.Orange, "RoleNameHermione"));
+        hourglassCooldown = new CustomNumberOption(num++, "hourglassCooldown", 20f, 40f, 10f, 2.5f, CooldownFormat);
+        hourglassTimer = new CustomNumberOption(num++, "hourglassTimer", 5f, 5f, 30f, 1f, CooldownFormat);
+
+
+        ItemSettings = new CustomHeaderOption(num++, "ItemSettings");
+
+        beerDuration = new CustomNumberOption(num++, "beerDuration", 5f, 5f, 30f, 1f, CooldownFormat);
+        mapDuration = new CustomNumberOption(num++, "mapDuration", 5f, 5f, 30f, 1f, CooldownFormat);
+    }
+
+    public void ReloadSettings()
+    {
+        EnableModRole = enableModRole.Get();
+
+        EnableVoldemort = (int)VoldemortOn.Get();
+        EnableBellatrix = (int)BellatrixOn.Get();
+        EnableHarry = (int)HarryOn.Get();
+        EnableHermione = (int)HermioneOn.Get();
+        EnableRon = (int)RonOn.Get();
+
+
+        OrderOfTheImp = orderOfTheImp.Get();
+        SpellsInVents = spellsInVents.Get();
+        DefensiveDuelistCooldown = defensiveDuelistCooldown.Get();
+        InvisCloakCooldown = invisCloakCooldown.Get();
+        HourglassCooldown = hourglassCooldown.Get();
+        CrucioCooldown = crucioCooldown.Get();
+        SeparateCooldowns = !separateCooldowns.Get();
+        ImperioDuration = imperioDuration.Get();
+        RandomGameStartPosition = randomGameStartPosition.Get();
+        MapDuration = mapDuration.Get();
+        DefensiveDuelistDuration = defensiveDuelistDuration.Get();
+        InvisCloakDuration = invisCloakDuration.Get();
+        HourglassTimer = hourglassTimer.Get();
+        BeerDuration = beerDuration.Get();
+        CrucioDuration = crucioDuration.Get();
     }
 }
